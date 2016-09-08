dbb560ffbb0146e5e57cd99d6bdb5d5161ad5a58
Reza Moallemi <rm.allmails@gmail.com>
2016-09-08 10:28:30 +0430

    Init repo

---
+++ app/src/main/java/ir/cafebazaar/workshop/MainActivity.java
@@ -0,0 +1,13 @@
+package ir.cafebazaar.workshop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
